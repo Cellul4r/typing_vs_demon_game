5306e41bc50ab134d5e6db48966d5f36a5db667a
cellul4r <seriousme.com@gmail.com>
2025-03-01 18:45:20 +0700

    add new sound

--- src/sound/SoundManager.java
+++ src/sound/SoundManager.java
@@ -6,13 +6,17 @@ package sound;
  */
 public class SoundManager {
     
-    Sound sound[] = new Sound[6];
+    Sound sound[] = new Sound[10];
     public static final int TITLE_MUSIC = 0;
     public static final int PLAY_MUSIC = 1;
     public static final int ENEMY_DEAD = 2;
     public static final int GUN_SOUND = 3;
     public static final int ENEMY_SOUND = 4;
     public static final int WRONG_TYPED = 5;
+    public static final int PLAYER_LOSE = 6;
+    public static final int HEALING_SOUND = 7;
+    public static final int FREEZE_SOUND = 8;
+    public static final int BOMB_SOUND = 9;
     
     public SoundManager(){
         //Wav file only
@@ -22,6 +26,10 @@ public class SoundManager {
         sound[3] = new Sound("/resource/sound_res/gun_sound.wav", 3.0f);
         sound[4] = new Sound("/resource/sound_res/enemy_sound.wav", -25.0f);
         sound[5] = new Sound("/resource/sound_res/wrong_typed.wav", 6.0f);
+        sound[6] = new Sound("/resource/sound_res/player_lose_sound.wav", 0.0f);
+        sound[7] = new Sound("/resource/sound_res/healing_sound.wav", 0.0f);
+        sound[8] = new Sound("/resource/sound_res/freeze_sound.wav", 0.0f);
+        sound[9] = new Sound("/resource/sound_res/bomb_sound.wav", 0.0f);
     }
     
     public void playSoundEffect(int i) {
