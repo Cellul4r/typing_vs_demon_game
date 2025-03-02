28bb01749303574fb070a2d473e2f7362b08e81c
Cellul4r <seriousme.com@gmail.com>
2025-03-02 12:09:16 +0700

    Update new algorithm to set speed for enemy

--- src/entity/Enemy.java
+++ src/entity/Enemy.java
@@ -93,7 +93,7 @@ public abstract class Enemy extends WordObject {
     
     protected void setSpeed(int level) {
         speedFactor = getSpeedFactor(gp.difficulty);
-        this.speed = baseSpeed + (int)(baseSpeed * level * speedFactor);
+        this.speed = Math.min(maxSpeed, baseSpeed + (int)(baseSpeed * level * speedFactor));
         System.out.println(speed + " " + level);
     }
     
